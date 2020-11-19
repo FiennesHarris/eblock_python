c931d076ab9f427f7e1d275f373e2f805f3d8e0c
fiennes harris <65114358+FiennesHarris@users.noreply.github.com>
2020-11-19 09:33:48 +0000

    Add files via upload

---
+++ problem5.py
@@ -0,0 +1,30 @@
+# Fix this code to give the following output:
+# It costs an average of £ 2.89 to feed each animal
+
+
+Farmer = "Simon"
+pigs = 54
+cows = 40
+chickens = 60
+totalAnimals  = (pigs + cows + chickens)
+foodCost = 445
+foodCostAv = (foodCost / totalAnimals)
+
+print ("There are a total of", totalAnimals, "animals on",
+Farmer+"'s farm")
+
+print (f"It costs an average of £{foodCostAv} to feed each animal")
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
