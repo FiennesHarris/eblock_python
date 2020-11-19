83c184d718808e3e8fec8fe63d3065aa2b0dfca9
fiennes harris <65114358+FiennesHarris@users.noreply.github.com>
2020-11-19 09:37:21 +0000

    Add files via upload

---
+++ problem6.py
@@ -0,0 +1,39 @@
+
+
+# Correct this code to print
+
+#Room Length: 10
+#Room Width 5
+#Area 50
+#5.0 of Blue paint are needed
+
+color = "Blue"
+roomLength = 10
+roomWidth =  5
+print ("Room Length:", roomLength)
+
+print ("Room Width", roomWidth)
+area = (roomLength * roomWidth)
+print ("Area", area)
+tinCoverage = 10
+# each tin covers 10m
+paintTins = area / tinCoverage
+print(f"{paintTins} of blue paint are needed")
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ problem7.py
@@ -0,0 +1,38 @@
+
+'''
+Create a program to print out the average
+number of sweets that each kid gets.
+
+The output should be:
+Each kid receives 80.0 sweets each
+
+use these variables to start:
+
+'''
+
+kids = 10
+sweets = 800
+receives = sweets / kids
+print(f"each kid recieves {receives} sweets each")
+
+
+
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
