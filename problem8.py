ae0301b1ace902cb3acd5abf1a5f4c408d74da89
fiennes harris <65114358+FiennesHarris@users.noreply.github.com>
2020-11-19 09:36:54 +0000

    Add files via upload

---
+++ problem8.py
@@ -0,0 +1,42 @@
+
+'''
+Create a program to print out volume of a cube.
+
+The program needs to have variables for
+length a width and a height
+It will multiply each of these together to get the volume
+It will then output all the values.
+
+Example Output:
+
+Length: 2
+Width: 3
+Height: 2
+The Volume is: 12
+
+'''
+
+length = 2
+width = 3
+height = 2
+volume = (length * width * height)
+print(f"the volume is {volume}")
+
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
